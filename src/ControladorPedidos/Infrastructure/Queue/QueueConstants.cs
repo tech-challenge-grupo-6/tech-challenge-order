5c555ebba46fbc80864f2f95a0f4befbb4fd68a4
Vitor de Souza <vitordesolza@gmail.com>
2024-07-13 15:27:05 -0300

    Change queue name

--- src/ControladorPedidos/Infrastructure/Queue/QueueConstants.cs
+++ src/ControladorPedidos/Infrastructure/Queue/QueueConstants.cs
@@ -4,5 +4,5 @@ public static class QueueConstants
 {
     public const string PedidoCriado = "pedido-criado";
     public const string PedidoAtualizado = "pedido-atualizado";
-    public const string PedidoCozinha = "tc_status_queue.fifo";
+    public const string PedidoCozinha = "tc_status_queue";
 }
