1ecc8f9d9cab8d05106850c00c19b8a71339242e
Vitor de Souza <vitordesolza@gmail.com>
2024-07-13 14:06:02 -0300

    Adding status pago

--- src/ControladorPedidos/Application/Pedidos/Queries/PegarPedidoQuery.cs
+++ src/ControladorPedidos/Application/Pedidos/Queries/PegarPedidoQuery.cs
@@ -3,11 +3,11 @@ using MediatR;
 
 namespace ControladorPedidos.Application.Pedidos.Queries;
 
-public record PegarPedidoQueryResponse(Guid Id, Guid? ClienteId, Status Status, IEnumerable<Guid> ProdutosIds, double ValorTotal)
+public record PegarPedidoQueryResponse(Guid Id, Guid? ClienteId, Status Status, IEnumerable<Guid> ProdutosIds, double ValorTotal, bool Pago)
 {
     public static explicit operator PegarPedidoQueryResponse(Pedido pedido)
     {
-        return new PegarPedidoQueryResponse(pedido.Id, pedido.ClienteId, pedido.Status, pedido.Produtos.Select(p => p.Id), pedido.ValorTotal);
+        return new PegarPedidoQueryResponse(pedido.Id, pedido.ClienteId, pedido.Status, pedido.Produtos.Select(p => p.Id), pedido.ValorTotal, pedido.Pago);
     }
 }
 public record PegarPedidoQuery(Guid Id) : IRequest<PegarPedidoQueryResponse>;
